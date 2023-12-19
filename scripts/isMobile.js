2100e53d467778618ebae94beae6d00a44ea70a2
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-12-19 16:45:41 -0700

    Added mobile responsive images

--- scripts/isMobile.js
+++ scripts/isMobile.js
@@ -5,13 +5,16 @@ function isMobile() {
 const titleElement = document.getElementById('titleContainer');
 const headerElement = document.getElementById('headerContainer');
 const bodyElement = document.getElementById('bodyContainer');
+const imageElement = document.getElementById('imageContainer');
 
 if (isMobile()) {
     titleElement.style.maxWidth = '100%';
     headerElement.style.maxWidth = '100%';
     bodyElement.style.maxWidth = '100%';
+    imageElement.style.maxWidth = '75%'
 } else {
     titleElement.style.maxWidth = '75%';
     headerElement.style.maxWidth = '75%';
     bodyElement.style.maxWidth = '75%';
+    imageElement.style.maxWidth = '50%'
 }
